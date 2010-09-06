006902c3d5575dbf026dca76ac5e7939ee4d318a
Jozef Sovcik <jozef@sovcik.net>
2010-09-07 01:13:17 +0200

    fixes for custom query support

--- examples/demo_aws_sdb.sh
+++ examples/demo_aws_sdb.sh
@@ -35,51 +35,52 @@ fi
 # create some items in test domain
 echo "-------------------------------"
 echo "Adding 4 items to $DBDOMAIN"
-ruby reg2rep.rb -o ~/reg2rep.log --add $DBDOMAIN item1 "key1:value1;key2:value2;key3:value3" --id $AWS_ACCESS_KEY --secret $AWS_SECRET_KEY --address $AWS_ADDRESS
+ruby ../reg2rep.rb -o ~/reg2rep.log --add $DBDOMAIN item1 "key1:value1;key2:value2;key3:value3" --id $AWS_ACCESS_KEY --secret $AWS_SECRET_KEY --address $AWS_ADDRESS
 
-ruby reg2rep.rb -o ~/reg2rep.log --add $DBDOMAIN item2 "key1:value1;key3:value3" --id $AWS_ACCESS_KEY --secret $AWS_SECRET_KEY --address $AWS_ADDRESS
+ruby ../reg2rep.rb -o ~/reg2rep.log --add $DBDOMAIN item2 "key1:value1;key3:value3" --id $AWS_ACCESS_KEY --secret $AWS_SECRET_KEY --address $AWS_ADDRESS
 
-ruby reg2rep.rb -o ~/reg2rep.log --add $DBDOMAIN item3 "key1:value1;key2:value2" --id $AWS_ACCESS_KEY --secret $AWS_SECRET_KEY --address $AWS_ADDRESS
+ruby ../reg2rep.rb -o ~/reg2rep.log --add $DBDOMAIN item3 "key1:value1;key2:value2" --id $AWS_ACCESS_KEY --secret $AWS_SECRET_KEY --address $AWS_ADDRESS
 
-ruby reg2rep.rb -o ~/reg2rep.log --add $DBDOMAIN item4 "key2:value2;key3:value3" --id $AWS_ACCESS_KEY --secret $AWS_SECRET_KEY --address $AWS_ADDRESS
+ruby ../reg2rep.rb -o ~/reg2rep.log --add $DBDOMAIN item4 "key2:value2;key3:value3" --id $AWS_ACCESS_KEY --secret $AWS_SECRET_KEY --address $AWS_ADDRESS
 
 
 # list inserted items as table
 echo "-------------------------------"
 echo "Showing inserted items in table"
-ruby reg2rep.rb -o ~/reg2rep.log --list $DBDOMAIN table --id $AWS_ACCESS_KEY --secret $AWS_SECRET_KEY --address $AWS_ADDRESS
+ruby ../reg2rep.rb -o ~/reg2rep.log --list $DBDOMAIN table --id $AWS_ACCESS_KEY --secret $AWS_SECRET_KEY --address $AWS_ADDRESS
 
 
 # modify one item
 echo "-------------------------------"
-echo "Update 'item3' 'key1' to value 'value1111'"
-ruby reg2rep.rb -o ~/reg2rep.log --update $DBDOMAIN item3 "key1:value1111" --id $AWS_ACCESS_KEY --secret $AWS_SECRET_KEY --address $AWS_ADDRESS
+echo "Update 'item2' 'key3' to value 'value1111'"
+ruby ../reg2rep.rb -o ~/reg2rep.log --update $DBDOMAIN item2 "key3:value1111" --id $AWS_ACCESS_KEY --secret $AWS_SECRET_KEY --address $AWS_ADDRESS
 
 
 # show items as hash
 echo "-------------------------------"
 echo "Showing inserted items as hash"
-ruby reg2rep.rb -o ~/reg2rep.log --list $DBDOMAIN hash --id $AWS_ACCESS_KEY --secret $AWS_SECRET_KEY --address $AWS_ADDRESS
+ruby ../reg2rep.rb -o ~/reg2rep.log --list $DBDOMAIN hash --id $AWS_ACCESS_KEY --secret $AWS_SECRET_KEY --address $AWS_ADDRESS
 
 # show items using custom query
 echo "-------------------------------"
 echo "Showing subset of inserted items using custom query"
-ruby reg2rep.rb -o ~/reg2rep.log --list $DBDOMAIN hash --query "select key1 from %domain% where key3 = 'value3'" --id $AWS_ACCESS_KEY --secret $AWS_SECRET_KEY --address $AWS_ADDRESS 
+# see http://developer.amazonwebservices.com/connect/entry.jspa?externalID=1231 for more info about SimpleDB queries
+ruby ../reg2rep.rb -o ~/reg2rep.log --list $DBDOMAIN hash --query "select key1 from %domain% where key3 = 'value3'" --id $AWS_ACCESS_KEY --secret $AWS_SECRET_KEY --address $AWS_ADDRESS 
 
 exit 0
 
 # delete two items
 echo "-------------------------------"
 echo "Deleting 'item1' and 'item3'"
-ruby reg2rep.rb -o ~/reg2rep.log --delete $DBDOMAIN item1 --id $AWS_ACCESS_KEY --secret $AWS_SECRET_KEY --address $AWS_ADDRESS
+ruby ../reg2rep.rb -o ~/reg2rep.log --delete $DBDOMAIN item1 --id $AWS_ACCESS_KEY --secret $AWS_SECRET_KEY --address $AWS_ADDRESS
 
-ruby reg2rep.rb -o ~/reg2rep.log --delete $DBDOMAIN item3 --id $AWS_ACCESS_KEY --secret $AWS_SECRET_KEY --address $AWS_ADDRESS
+ruby ../reg2rep.rb -o ~/reg2rep.log --delete $DBDOMAIN item3 --id $AWS_ACCESS_KEY --secret $AWS_SECRET_KEY --address $AWS_ADDRESS
 
 
 # show only item names
 echo "-------------------------------"
 echo "Showing only item identifiers"
-ruby reg2rep.rb -o ~/reg2rep.log --list $DBDOMAIN items --id $AWS_ACCESS_KEY --secret $AWS_SECRET_KEY --address $AWS_ADDRESS
+ruby ../reg2rep.rb -o /root/reg2rep.log --list $DBDOMAIN items --id $AWS_ACCESS_KEY --secret $AWS_SECRET_KEY --address $AWS_ADDRESS
 
 
 
